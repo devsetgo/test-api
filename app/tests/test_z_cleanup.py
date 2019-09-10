a134a516bc4d0a6d401e04fef5a523f7c2ccb3a1
devsetgo <mikeryan56@gmail.com>
2019-09-10 09:32:43 -0400

    adding file creation cleanup

---
+++ app/tests/test_z_cleanup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# import json
+# import csv
+import datetime
+import os
+import time
+import unittest
+from unittest import mock
+
+import pytest
+
+from com_lib.file_functions import (
+    create_sample_files,
+    get_data_directory_list,
+    open_csv,
+    open_json,
+    open_text,
+    save_csv,
+    save_json,
+    save_text,
+    delete_file,
+)
+
+
+class test_file_delete(unittest.TestCase):
+    def test_clean_up(self):
+        files = [
+            "test_1_error.json",
+            "test_1_todo.json",
+            "test_data_test_user.json",
+            "test_data_todos.json",
+            "test_data_users.json",
+        ]
+        for f in files:
+            delete_file(f)
