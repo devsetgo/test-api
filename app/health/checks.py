0ee6f1c1b076404c5082cfef38c7ed978de19878
devsetgo <mikeryan56@gmail.com>
2019-09-10 22:10:13 -0400

    Updating todo

--- app/health/checks.py
+++ app/health/checks.py
@@ -9,7 +9,8 @@ import time
 
 import psutil
 
-# TODO: Make a status that gets app uptime
+# @todo Make a status that gets app uptime
+# @body how to get uptime of app, not running os. Such as dockerimage or how long the code has been running.
 # def get_status() -> dict:
 #     uptime = seconds_elapsed()
 #     result: dict = {
