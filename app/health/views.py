346b2a163f77a60eb6e0f2ef342fba9ff2506a15
devsetgo <mikeryan56@gmail.com>
2019-09-20 18:44:08 -0400

    adding class

--- app/health/views.py
+++ app/health/views.py
@@ -15,57 +15,56 @@ router = APIRouter()
 # TODO: Determine method to get application uptime
 
 
-@router.get("/", tags=["system-health"])
-async def health_main() -> dict:
-    """
-    GET status, uptime, and current datetime
+class HealthChecks:
+    @router.get("/", tags=["system-health"])
+    async def health_main() -> dict:
+        """
+        GET status, uptime, and current datetime
 
-    Returns:
-        dict -- [status: UP, uptime: seconds current_datetime: datetime.now]
-    """
-    result: dict = {"status": "UP"}
-    return result
-
-
-@router.get("/system-info", tags=["system-health"])
-async def health_details() -> dict:
-    """
-    GET Request for CPU and process data
-
-    Returns:
-        dict -- [current_datetime: datetime.now, system information: Python and System Information]
-    """
-
-    try:
-        system_info = get_cpu_info()
-        result: dict = {
-            "current_datetime": str(datetime.datetime.now()),
-            "system_info": system_info,
-        }
-        logger.info(f"GET system info")
+        Returns:
+            dict -- [status: UP, uptime: seconds current_datetime: datetime.now]
+        """
+        result: dict = {"status": "UP"}
         return result
-        # TODO: make more specific Exception
-        # BODY: Exception is generic and should be more specific or removed.
-    except Exception as e:
-        logger.error(f"Error: {e}")
 
+    @router.get("/system-info", tags=["system-health"])
+    async def health_details() -> dict:
+        """
+        GET Request for CPU and process data
 
-@router.get("/processes", tags=["system-health"])
-async def health_processes() -> dict:
-    """
-    GET running processes and filter by python processes
+        Returns:
+            dict -- [current_datetime: datetime.now, system information: Python and System Information]
+        """
 
-    Returns:
-        dict -- [pid, name, username]
-    """
-    try:
-        system_info = get_processes()
-        result: dict = {
-            "current_datetime": str(datetime.datetime.now()),
-            # "note": "this is filter to only return, python, gunicorn, uvicorn, hypercorn, and daphne pids for security",
-            "running_processes": system_info,
-        }
-        logger.info(f"GET processes")
-        return result
-    except Exception as e:
-        logger.error(f"Error: {e}")
+        try:
+            system_info = get_cpu_info()
+            result: dict = {
+                "current_datetime": str(datetime.datetime.now()),
+                "system_info": system_info,
+            }
+            logger.info(f"GET system info")
+            return result
+            # TODO: make more specific Exception
+            # BODY: Exception is generic and should be more specific or removed.
+        except Exception as e:
+            logger.error(f"Error: {e}")
+
+    @router.get("/processes", tags=["system-health"])
+    async def health_processes() -> dict:
+        """
+        GET running processes and filter by python processes
+
+        Returns:
+            dict -- [pid, name, username]
+        """
+        try:
+            system_info = get_processes()
+            result: dict = {
+                "current_datetime": str(datetime.datetime.now()),
+                # "note": "this is filter to only return, python, gunicorn, uvicorn, hypercorn, and daphne pids for security",
+                "running_processes": system_info,
+            }
+            logger.info(f"GET processes")
+            return result
+        except Exception as e:
+            logger.error(f"Error: {e}")
