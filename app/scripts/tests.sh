3b2d81274b42aa73f432e8981b4ef1711646edf5
devsetgo <mikeryan56@gmail.com>
2019-09-20 19:51:57 -0400

    adding verbose statement

--- app/scripts/tests.sh
+++ app/scripts/tests.sh
@@ -9,7 +9,8 @@ set -x
 pre-commit run -a
 
 # bash scripts/test.sh --cov-report=html ${@}
-python3 -m pytest
+# python3 -m pytest
+python3 -m pytest -v -s
 
 # create coverage-badge
 coverage-badge -o ../coverage.svg -f
