24ec865c3f75e9c7d539d223bf3fb26158e31406
devsetgo <mikeryan56@gmail.com>
2019-10-26 17:18:30 -0400

    updates to add testfiles for test_tools

--- app/tests/test_tools.py
+++ app/tests/test_tools.py
@@ -27,9 +27,9 @@ directory_to__files: str = "data"
 class Test(unittest.TestCase):
     # xml endpoint test
     def test_xml_four_hundred(self):
-        file_directory = f"{directory_to__files}/json"
+        file_directory = f"{directory_to__files}/testfiles"
         directory_path = Path.cwd().joinpath(file_directory)
-        file_path = f"{directory_path}/sample.json"
+        file_path = f"{directory_path}/test_sample.json"
 
         url = f"/api/v1/tools/xml-json"
         files = {"myfile": open(file_path, "r")}
@@ -37,9 +37,9 @@ class Test(unittest.TestCase):
         assert response.status_code == 400
 
     def test_xml(self):
-        file_directory = f"{directory_to__files}/text"
+        file_directory = f"{directory_to__files}/testfiles"
         directory_path = Path.cwd().joinpath(file_directory)
-        file_path = f"{directory_path}/test.xml"
+        file_path = f"{directory_path}/test_sample.xml"
 
         url = f"/api/v1/tools/xml-json"
         files = {"myfile": open(file_path, "r")}
@@ -47,9 +47,9 @@ class Test(unittest.TestCase):
         assert response.status_code == 200
 
     def test_xml_error(self):
-        file_directory = f"{directory_to__files}/text"
+        file_directory = f"{directory_to__files}/testfiles"
         directory_path = Path.cwd().joinpath(file_directory)
-        file_path = f"{directory_path}/sample_bad.xml"
+        file_path = f"{directory_path}/test_sample_bad.xml"
 
         url = f"/api/v1/tools/xml-json"
         files = {"myfile": open(file_path, "r")}
@@ -58,9 +58,9 @@ class Test(unittest.TestCase):
 
     # json endpoint test
     def test_json_four_hundred(self):
-        file_directory = f"{directory_to__files}/text"
+        file_directory = f"{directory_to__files}/testfiles"
         directory_path = Path.cwd().joinpath(file_directory)
-        file_path = f"{directory_path}/sample.xml"
+        file_path = f"{directory_path}/test_sample.xml"
 
         url = f"/api/v1/tools/json-xml"
         files = {"myfile": open(file_path, "r")}
@@ -68,9 +68,9 @@ class Test(unittest.TestCase):
         assert response.status_code == 400
 
     def test_json(self):
-        file_directory = f"{directory_to__files}/json"
+        file_directory = f"{directory_to__files}/testfiles"
         directory_path = Path.cwd().joinpath(file_directory)
-        file_path = f"{directory_path}/sample.json"
+        file_path = f"{directory_path}/test_sample.json"
 
         url = f"/api/v1/tools/json-xml"
         files = {"myfile": open(file_path, "r")}
@@ -78,9 +78,9 @@ class Test(unittest.TestCase):
         assert response.status_code == 200
 
     def test_json_error(self):
-        file_directory = f"{directory_to__files}/json"
+        file_directory = f"{directory_to__files}/testfiles"
         directory_path = Path.cwd().joinpath(file_directory)
-        file_path = f"{directory_path}/sample_bad.json"
+        file_path = f"{directory_path}/test_sample_bad.json"
 
         url = f"/api/v1/tools/json-xml"
         files = {"myfile": open(file_path, "r")}
