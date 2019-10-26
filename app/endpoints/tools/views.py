6b6804126d0e7e167c8beef315eb1d086a681943
devsetgo <mikeryan56@gmail.com>
2019-10-26 10:51:44 -0400

    adding xml tools

--- app/endpoints/tools/views.py
+++ app/endpoints/tools/views.py
@@ -1,8 +1,21 @@
 # -*- coding: utf-8 -*-
 import asyncio
 import time
+import json
 
-from fastapi import APIRouter, FastAPI, Header, HTTPException, Path, Query,File, Form, UploadFile
+from fastapi import (
+    APIRouter,
+    FastAPI,
+    Header,
+    HTTPException,
+    Path,
+    Query,
+    File,
+    Form,
+    UploadFile,
+    
+)
+from starlette.responses import Response
 from loguru import logger
 
 from xmltodict import parse as xml_parse, unparse as xml_unparse
@@ -10,20 +23,42 @@ from xmltodict import parse as xml_parse, unparse as xml_unparse
 router = APIRouter()
 
 
-@router.post("/convert-to/xml")
+@router.post("/xml-json")
 async def convert_xml(
-    file: bytes = File(...), 
-    # file: UploadFile = File(...),
+    # file: bytes = File(...),
+    myfile: UploadFile = File(...),
 ):
+    if myfile.content_type != "text/xml":
+        error_exception = f"API requires text/xml, but file {myfile.filename} is {myfile.content_type}"
+        raise HTTPException(status_code=400, detail=error_exception)
 
-    result = xml_parse(file)
-    return result
+    try:
+        
+        contents = await myfile.read()      
+        result = xml_parse(contents, encoding="utf-8", process_namespaces=True)
+        logger.info(f"file {myfile.filename} converted to JSON")
+        return result
 
-@router.post("/convert-to/json")
+    except Exception as e:
+        logger.critical(f'error: {e}')
+    #
+
+
+@router.post("/json-xml")
 async def convert_json(
-    file: bytes = File(...), 
-    # file: UploadFile = File(...),
+    # myfile: bytes = File(...),
+    myfile: UploadFile = File(...),
 ):
+    if myfile.content_type != "application/json":
+        error_exception = f"API requires application/json, but file {myfile.filename} is {myfile.content_type}"
+        raise HTTPException(status_code=400, detail=error_exception)
+        
+    try:
+        content = await myfile.read()
+        new_dict = json.loads(content.decode('utf8'))
+        result = xml_unparse(new_dict, pretty = True)
+        logger.info(f"file {myfile.filename} converted to xml")
+        return Response(content=result, media_type="application/xml")
 
-    result = xml_unparse(file)
-    return result
+    except Exception as e:
+        logger.critical(f"error: {e}")
