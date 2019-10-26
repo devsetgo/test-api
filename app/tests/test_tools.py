135c1125ec06befbec922646e6ac090ad7699223
devsetgo <mikeryan56@gmail.com>
2019-10-26 15:11:11 -0400

    adding tests for tools xml and json

--- app/tests/test_tools.py
+++ app/tests/test_tools.py
@@ -25,42 +25,64 @@ directory_to__files: str = "data"
 
 
 class Test(unittest.TestCase):
-    def test_xml_error(self):
+    # xml endpoint test
+    def test_xml_four_hundred(self):
         file_directory = f"{directory_to__files}/json"
         directory_path = Path.cwd().joinpath(file_directory)
         file_path = f"{directory_path}/sample.json"
 
         url = f"/api/v1/tools/xml-json"
-        files = {"files": open(file_path, "rb")}
+        files = {"myfile": open(file_path, "r")}
         response = client.post(url, files=files)
-        assert response.status_code == 500
+        assert response.status_code == 400
 
-    # def test_users_post(self):
-    #     test_user = user_test_info()
-    #     save_json("test_data_test_user.json", test_user)
-    #     url = f"/api/v1/users/create/?delay=1"
+    def test_xml(self):
+        file_directory = f"{directory_to__files}/text"
+        directory_path = Path.cwd().joinpath(file_directory)
+        file_path = f"{directory_path}/test.xml"
 
-    #     response = client.post(url, json=test_user)
-    #     assert response.status_code == 200
-    #     data = response.json()
+        url = f"/api/v1/tools/xml-json"
+        files = {"myfile": open(file_path, "r")}
+        response = client.post(url, files=files)
+        assert response.status_code == 200
 
-    #     user_data = {
-    #         "userId": data["userId"],
-    #         "user_name": data["user_name"],
-    #         "password": test_user["password"],
-    #     }
+    def test_xml_error(self):
+        file_directory = f"{directory_to__files}/text"
+        directory_path = Path.cwd().joinpath(file_directory)
+        file_path = f"{directory_path}/sample_bad.xml"
 
-    #     save_json("test_data_users.json", user_data)
+        url = f"/api/v1/tools/xml-json"
+        files = {"myfile": open(file_path, "r")}
+        response = client.post(url, files=files)
+        assert response.status_code == 400
 
-    # def test_users_post_two(self):
-    #     test_user = user_test_info()
-    #     url = f"/api/v1/users/create/?delay=1"
+    # json endpoint test
+    def test_json_four_hundred(self):
+        file_directory = f"{directory_to__files}/text"
+        directory_path = Path.cwd().joinpath(file_directory)
+        file_path = f"{directory_path}/sample.xml"
 
-    #     response = client.post(url, json=test_user)
-    #     assert response.status_code == 200
+        url = f"/api/v1/tools/json-xml"
+        files = {"myfile": open(file_path, "r")}
+        response = client.post(url, files=files)
+        assert response.status_code == 400
 
+    def test_json(self):
+        file_directory = f"{directory_to__files}/json"
+        directory_path = Path.cwd().joinpath(file_directory)
+        file_path = f"{directory_path}/sample.json"
 
-# files = {'upload_file': open('file.txt','rb')}
-# values = {'DB': 'photcat', 'OUT': 'csv', 'SHORT': 'short'}
+        url = f"/api/v1/tools/json-xml"
+        files = {"myfile": open(file_path, "r")}
+        response = client.post(url, files=files)
+        assert response.status_code == 200
 
-# r = requests.post(url, files=files, data=values)
+    def test_json_error(self):
+        file_directory = f"{directory_to__files}/json"
+        directory_path = Path.cwd().joinpath(file_directory)
+        file_path = f"{directory_path}/sample_bad.json"
+
+        url = f"/api/v1/tools/json-xml"
+        files = {"myfile": open(file_path, "r")}
+        response = client.post(url, files=files)
+        assert response.status_code == 400
