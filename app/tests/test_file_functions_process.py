9174f79a02dfbb24abe9d59da792416f3e1b3c04
mike ryan <mikeryan56@gmail.com>
2020-02-09 17:51:31 -0500

    removing commented out code

--- app/tests/test_file_functions_process.py
+++ app/tests/test_file_functions_process.py
@@ -35,28 +35,24 @@ class Test(unittest.TestCase):
     def test_open_json_no_file(self):
         file_named = "no_file_name.json"
         with pytest.raises(Exception):
-            # assert open_json(file_named)
             result = open_json(file_named)
             assert result is None
 
     def test_open_json_exception_not_str(self):
         file_named = ["a", "list"]
         with pytest.raises(Exception):
-            # assert open_json(file_named)
             result = open_json(file_named)
             assert result is None
 
     def test_open_json_exception_slash_win(self):
         file_named = "\this_is_not_right_json"
         with pytest.raises(Exception):
-            # assert open_json(file_named)
             result = open_json(file_named)
             assert result is None
 
     def test_open_json_exception_slash_linux(self):
         file_named = "//this_is_not_right_json"
         with pytest.raises(Exception):
-            # assert open_json(file_named)
             result = open_json(file_named)
             assert result is None
 
@@ -68,7 +64,6 @@ class Test(unittest.TestCase):
     def test_open_csv_no_file(self):
         file_named = "no_file_name.csv"
         with pytest.raises(Exception):
-            # assert open_csv(file_named)
             result = open_csv(file_named)
             assert result is None
 
@@ -80,14 +75,12 @@ class Test(unittest.TestCase):
     def test_open_csv_exception_slash_win(self):
         file_named = "\this_is_not_right_csv"
         with pytest.raises(Exception):
-            # assert open_csv(file_named)
             result = open_csv(file_named)
             assert result is None
 
     def test_open_csv_exception_slash_linux(self):
         file_named = "//this_is_not_right_csv"
         with pytest.raises(FileNotFoundError):
-            # assert open_csv(file_named)
             result = open_csv(file_named)
             assert result is None
 
@@ -99,28 +92,24 @@ class Test(unittest.TestCase):
     def test_open_txt_no_file(self):
         file_named = "no_file_name.html"
         with pytest.raises(Exception):
-            # assert open_text(file_named)
             result = open_text(file_named)
             assert result is None
 
     def test_open_txt_exception_not_str(self):
         file_named = ["a", "list"]
         with pytest.raises(Exception):
-            # assert open_text(file_named)
             result = open_text(file_named)
             assert result is None
 
     def test_open_txt_exception_slash_win(self):
         file_named = "\this_is_not_right_txt"
         with pytest.raises(Exception):
-            # assert open_text(file_named)
             result = open_text(file_named)
             assert result is None
 
     def test_open_txt_exception_slash_linux(self):
         file_named = "//this_is_not_right_txt"
         with pytest.raises(Exception):
-            # assert open_text(file_named)
             result = open_text(file_named)
             assert result is None
 
@@ -139,6 +128,5 @@ class Test(unittest.TestCase):
     def test_get_data_directory_typeerror(self):
         directory = {"csv"}
         with pytest.raises(Exception):
-            # assert get_data_directory_list(directory)
             result = get_data_directory_list(directory)
             assert result is None
