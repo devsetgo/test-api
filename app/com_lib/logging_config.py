a18a977aef7fd7287a5fd191911f45d2dce612d8
mike ryan <mikeryan56@gmail.com>
2020-03-31 19:13:29 -0400

    adding  request parser

--- app/com_lib/logging_config.py
+++ app/com_lib/logging_config.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 from pathlib import Path
-
+import json
 from loguru import logger
-
+from loguru._defaults import LOGURU_FORMAT
 from settings import LOGURU_RETENTION
 from settings import LOGURU_ROTATION
+import logging, sys
 
 
 def config_logging():
+
     log_path = Path.cwd().joinpath("logfile").joinpath("app_log.log")
     logger.add(
         log_path,
@@ -17,7 +19,13 @@ def config_logging():
         rotation=LOGURU_ROTATION,
         retention=LOGURU_RETENTION,
         compression="zip",
-        serialize=True
+        # serialize=True,
     )
 
-    
+
+def request_parser(request_data):
+    client_host = request_data.client.host
+    meth = request_data.method
+    url_path = request_data.url.path
+    head = request_data.headers["user-agent"]
+    logger.info(f"{client_host} | {meth} | {url_path} | {head}")
