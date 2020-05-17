0ef57895cfb6d7a6f269cb1fe5920a7f59a139a4
mike ryan <mikeryan56@gmail.com>
2020-05-17 15:16:13 -0400

    moving

--- app/com_lib/db_setup.py
+++ app/com_lib/db_setup.py
@@ -2,14 +2,17 @@
 
 import databases
 from loguru import logger
-from sqlalchemy import Boolean
-from sqlalchemy import Column
-from sqlalchemy import Date
-from sqlalchemy import DateTime
-from sqlalchemy import MetaData
-from sqlalchemy import String
-from sqlalchemy import Table
-from sqlalchemy import create_engine
+from sqlalchemy import (
+    Boolean,
+    JSON,
+    Column,
+    Date,
+    DateTime,
+    MetaData,
+    String,
+    Table,
+    create_engine,
+)
 from sqlalchemy.pool import QueuePool
 
 from settings import SQLALCHEMY_DATABASE_URI
@@ -61,6 +64,13 @@ users = Table(
     Column("is_superuser", Boolean(), default=True),
 )
 
+email_service = Table(
+    "email_service",
+    metadata,
+    Column("email_id", String, primary_key=True),
+    Column("sent", Boolean(), default=False),
+    Column("email_content", JSON()),
+)
 
 todos = Table(
     "todos",
