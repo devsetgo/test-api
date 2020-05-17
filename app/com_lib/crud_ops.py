638ed47e4b210550af2e57b704cc540e5cbc1f01
mike ryan <mikeryan56@gmail.com>
2020-05-17 15:16:32 -0400

    start of centralizing CRUD ops

---
+++ app/com_lib/crud_ops.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+database simple functions. Pass query and where needed values and get result back
+"""
+
+import logging
+from loguru import logger
+from sqlalchemy.sql import text
+
+from com_lib.db_setup import database
+
+
+async def fetch_one_db(query):
+
+    result = await database.fetch_one(query)
+    logger.debug(str(result))
+    return result
+
+
+async def fetch_all_db(query):
+
+    result = await database.fetch_all(query)
+    logger.debug(str(result))
+    return result
+
+
+async def execute_one_db(query, values: dict):
+
+    result = await database.execute(query, values)
+    logger.debug(str(result))
+    return result
+
+
+async def execute_many_db(query, values: dict):
+
+    result = await database.execute_many(query, values)
+    logger.debug(str(result))
+    return result
