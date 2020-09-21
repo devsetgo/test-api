ea0f8bc88b7245944ce47a4679814180b9c4b6f7
devsetgo <mike@DESKTOP-6FCGHOC.localdomain>
2020-09-20 20:31:54 -0400

    bug fix converting string to int

--- app/gunicorn_cfg.py
+++ app/gunicorn_cfg.py
@@ -7,6 +7,8 @@ https://github.com/benoitc/gunicorn/blob/master/examples/example_config.py
 
 import multiprocessing
 
+from loguru import logger
+
 from settings import LOGURU_LOGGING_LEVEL, WORKERS
 
 # ip and port to bind
@@ -15,10 +17,11 @@ bind = "0.0.0.0:5000"
 # backlog = 2048
 # define number of workers by cores times two plus one
 # edit if you want to set a specific/limited amount of workers
+
 if WORKERS == 0 or WORKERS is None:
     workers = multiprocessing.cpu_count() * 2 + 1
 else:
-    workers = WORKERS
+    workers = int(WORKERS)
 # set worker class to uvicorn
 worker_class = "uvicorn.workers.UvicornWorker"
 
--- app/settings.py
+++ app/settings.py
@@ -48,7 +48,7 @@ if USE_ENV.lower() == "dotenv":
     LOGURU_ROTATION = config("LOGURU_ROTATION", default="10 MB")
     LOGURU_LOGGING_LEVEL = config("LOGURU_LOGGING_LEVEL", default="WARNING")
     ADD_DEFAULT_GROUP = config("ADD_DEFAULT_GROUP", default="True")
-    WORKERS = config("WORKERS", default=0)
+    WORKERS = int(config("WORKERS", default=0))
 else:
     logger.info(
         f"USE_ENV set to {USE_ENV}. Using os environmental settings for\
@@ -125,6 +125,7 @@ else:
     LOGURU_LOGGING_LEVEL = os.environ["LOGURU_LOGGING_LEVEL"]
     if LOGURU_LOGGING_LEVEL is None:
         LOGURU_LOGGING_LEVEL = "INFO"
-    WORKERS = os.environ["WORKERS"]
+    WORKERS = int(os.environ["WORKERS"])
+    
     if WORKERS is None or WORKERS == 0:
         WORKERS = 0
