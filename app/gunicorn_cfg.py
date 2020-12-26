a632722e8a712da22c2ac6ea2ac1ccc7576be9a5
mike <mikeryan56@gmail.com>
2020-12-25 19:47:08 -0500

    change of worker class

--- app/gunicorn_cfg.py
+++ app/gunicorn_cfg.py
@@ -7,9 +7,8 @@ https://github.com/benoitc/gunicorn/blob/master/examples/example_config.py
 
 import multiprocessing
 
-from loguru import logger
 
-from settings import LOGURU_LOGGING_LEVEL, WORKERS
+from settings import config
 
 # ip and port to bind
 bind = "0.0.0.0:5000"
@@ -18,16 +17,20 @@ bind = "0.0.0.0:5000"
 # define number of workers by cores times two plus one
 # edit if you want to set a specific/limited amount of workers
 
-if WORKERS == 0 or WORKERS is None:
+if config.workers == 0 or config.workers is None:
     workers = multiprocessing.cpu_count() * 2 + 1
 else:
-    workers = int(WORKERS)
-# set worker class to uvicorn
-worker_class = "uvicorn.workers.UvicornWorker"
+    workers = config.workers
 
-# loglevel - The granularity of log output
-# A string of "debug", "info", "warning", "error", "critical"
-loglevel = LOGURU_LOGGING_LEVEL.lower()
+# set worker class to uvicorn
+# worker_class = "uvicorn.workers.uvicornworker"
+worker_class = "uvicorn.workers.UvicornH11Worker"
+
+# loglevel - the granularity of log output
+# a string of "debug", "info", "warning", "error", "critical"
+loglevel = str(config.loguru_logging_level.lower())
+
+
 
 """
 A dictionary containing headers and values that the front-end proxy
