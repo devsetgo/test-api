67bf74c81171929533f4d33cd4a4e000e3289f82
mike <mikeryan56@gmail.com>
2020-12-25 19:52:00 -0500

    change to pydantic for settings management

--- app/settings.py
+++ app/settings.py
@@ -5,132 +5,56 @@ For local development, use a .env file to set
 environment variables.
 """
 
-import os
+# import os
 import secrets
+from datetime import datetime
+from functools import lru_cache
+# from starlette.config import Config
+from pydantic import BaseSettings,AnyUrl
 
-from loguru import logger
-from starlette.config import Config
-
-# get environment variables
-config = Config(".env")
-USE_ENV = config("USE_ENV", default="docker")
-
-SECRET_KEY = secrets.token_urlsafe(64)
+# from loguru import logger
 
 
-if USE_ENV.lower() == "dotenv":
-    logger.info(f"USE_ENV set to {USE_ENV}. Using .env file for external configuration")
-    # Application information
-    APP_VERSION = config("APP_VERSION", default="1.0.0")
-    OWNER = config("OWNER", default="Mike Ryan")
-    WEBSITE = config("WEBSITE", default="https://devsetgo.com")
-    LICENSE_TYPE = config("LICENSE_TYPE", default="MIT")
-    LICENSE_LINK = config(
-        "LICENSE_LINK", default="https://github.com/devsetgo/starlette-SRTDashboard"
-    )
+class Settings(BaseSettings):
+    # use_env = "dotenv"
+    title: str = "Test API"
+    description: str = "Example API to learn from."
+    app_version: str = "1.0.0"
+    owner: str = "Mike Ryan"
+    website: AnyUrl = "https://devsetgo.com"
+    license_type: str = "MIT"
+    license_link: AnyUrl = "https://github.com/devsetgo/test-api/blob/master/LICENSE"
+    # application configurations
+    host_domain: AnyUrl = "https://test-api.devsetgo.com"
+    release_env: str = "prd"
+    https_on: bool = True
+    prometheus_on: bool = True
+    database_type:str = "sqlite"
+    db_name:str= "sqlite_db/api.db"
+    sqlalchemy_database_uri: str ="sqlite:///sqlite_db/api.db"
+    add_default_group: bool = True
+    workers: int = 2
+    secret_key: str = str(secrets.token_urlsafe(4))
+    # demo data
+    create_sample_data: bool = True
+    number_tasks: int = 100
+    number_users: int = 100
+    number_groups: int = 100
+    # loguru settings
+    loguru_retention: str = "10 days"
+    loguru_rotation: str = "100 MB"
+    loguru_logging_level: str = "DEBUG"
+    # Config info
+    spew: bool = False
+    updated: datetime = datetime.utcnow()
 
-    # Demo Data
-    CREATE_SAMPLE_DATA = config("CREATE_SAMPLE_DATA", default=False)
-    NUMBER_TASKS = config("NUMBER_TASKS", default=0)
-    NUMBER_USERS = config("NUMBER_USERS", default=0)
-    NUMBER_GROUPS = config("NUMBER_GROUPS", default=0)
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
 
-    # Application Configurations
-    HOST_DOMAIN = config("HOST_DOMAIN", default="https://devsetgo.com")
-    RELEASE_ENV = config("RELEASE_ENV", default="prd")
-    HTTPS_ON = config("HTTPS_ON", default="True")
-    PROMETHEUS_ON = config("PROMETHEUS_ON", default="False")
-    SQLALCHEMY_DATABASE_URI = config(
-        "SQLALCHEMY_DATABASE_URI", default="sqlite:///sqlite_db/api.db"
-    )
+@lru_cache()
+def get_settings():
+    return Settings()
 
-    # Loguru settings
-    LOGURU_RETENTION = config("LOGURU_RETENTION", default="10 days")
-    LOGURU_ROTATION = config("LOGURU_ROTATION", default="10 MB")
-    LOGURU_LOGGING_LEVEL = config("LOGURU_LOGGING_LEVEL", default="WARNING")
-    ADD_DEFAULT_GROUP = config("ADD_DEFAULT_GROUP", default="True")
-    WORKERS = int(config("WORKERS", default=0))
-else:
-    logger.info(
-        f"USE_ENV set to {USE_ENV}. Using os environmental settings for\
-             external configuration"
-    )
-    # Application information
-    APP_VERSION = os.environ["APP_VERSION"]
-    logger.critical(APP_VERSION)
-    if APP_VERSION is None:
-        APP_VERSION = "0.0.1"
-
-    OWNER = os.environ["OWNER"]
-    if OWNER is None:
-        OWNER = "example"
-
-    WEBSITE = os.environ["WEBSITE"]
-    if WEBSITE is None:
-        WEBSITE = "http://www.example.com"
-
-    LICENSE_TYPE = os.environ["LICENSE_TYPE"]
-    if LICENSE_TYPE is None:
-        LICENSE_TYPE = "MIT"
-
-    LICENSE_LINK = os.environ["LICENSE_LINK"]
-    if LICENSE_LINK is None:
-        LICENSE_LINK = "http://www.example.com"
-
-    # Demo Data
-    CREATE_SAMPLE_DATA = os.environ["CREATE_SAMPLE_DATA"]
-    if CREATE_SAMPLE_DATA is None:
-        CREATE_SAMPLE_DATA = "False"
-
-    NUMBER_TASKS = os.environ["NUMBER_TASKS"]
-    if NUMBER_TASKS is None:
-        NUMBER_TASKS = 10
-    NUMBER_USERS = os.environ["NUMBER_USERS"]
-    if NUMBER_USERS is None:
-        NUMBER_USERS = 10
-
-    NUMBER_GROUPS = os.environ["NUMBER_USERS"]
-    if NUMBER_GROUPS is None:
-        NUMBER_GROUPS = 10
-
-    # Application Configurations
-    HOST_DOMAIN = os.environ["HOST_DOMAIN"]
-    if HOST_DOMAIN is None:
-        HOST_DOMAIN = "https://example.com"
-
-    RELEASE_ENV = os.environ["RELEASE_ENV"]
-    if RELEASE_ENV is None:
-        RELEASE_ENV = "prd"
-
-    HTTPS_ON = os.environ["HTTPS_ON"]
-    if HTTPS_ON is None:
-        HTTPS_ON = True
-
-    PROMETHEUS_ON = os.environ["PROMETHEUS_ON"]
-    if PROMETHEUS_ON is None:
-        PROMETHEUS_ON = False
-
-    ADD_DEFAULT_GROUP = os.environ["ADD_DEFAULT_GROUP"]
-    if ADD_DEFAULT_GROUP is None:
-        ADD_DEFAULT_GROUP = "False"
-
-    SQLALCHEMY_DATABASE_URI = os.environ["SQLALCHEMY_DATABASE_URI"]
-    if SQLALCHEMY_DATABASE_URI is None:
-        SQLALCHEMY_DATABASE_URI = "sqlite:///sqlite_db/api.db"
-
-    # Loguru settings
-    LOGURU_RETENTION = os.environ["LOGURU_RETENTION"]
-    if LOGURU_RETENTION is None:
-        LOGURU_RETENTION = "10 days"
-
-    LOGURU_ROTATION = os.environ["LOGURU_ROTATION"]
-    if LOGURU_ROTATION is None:
-        LOGURU_ROTATION = "100 MB"
-
-    LOGURU_LOGGING_LEVEL = os.environ["LOGURU_LOGGING_LEVEL"]
-    if LOGURU_LOGGING_LEVEL is None:
-        LOGURU_LOGGING_LEVEL = "INFO"
-    WORKERS = int(os.environ["WORKERS"])
-
-    if WORKERS is None or WORKERS == 0:
-        WORKERS = 0
+config = get_settings()
+# config = Settings()
