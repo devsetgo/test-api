14b656bd4c55b2a576cada6544ac81c775f43ced
devsetgo <mikeryan56@gmail.com>
2021-04-10 21:22:48 -0400

    test

--- src/scripts/tests.sh
+++ src/scripts/tests.sh
@@ -21,7 +21,7 @@ python3 -m pytest
 # create coverage-badge
 coverage-badge -o ../coverage.svg -f
 # mv ~/test-api/src/coverage.xml ~/test-api/coverage-reports/coverage.xml
-# sed -i "s/<source>\/home\/runner\/work\/repo<\/source>/<source>\/github\/workspace<\/source>/g" /home/runner/work/repo/src/coverage.xml
+sed -i "s/<source>\/home\/runner\/work\/repo<\/source>/<source>\/github\/workspace\/src<\/source>/g" /home/mike/test-api/src/coverage.xml
 # delete db
 # rm sqlite_db/api.db
 # echo "db removed"
