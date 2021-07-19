a476ae6d4f4e998272430c523906672f7c66b559
devsetgo <mikeryan56@gmail.com>
2021-07-18 20:16:02 -0400

    fixes and updates

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,9 +10,8 @@ echo 'Docker Build Python'
 docker build -t mikeryan56/test-api:$CAL_VER-python38 -t mikeryan56/test-api:latest -f ./dockerfiles/dockerfile_python .
 # echo "Running Docker Image"
 # docker run mikeryan56/test-api:$CAL_VER-python38
-# echo "Push"
-# docker push mikeryan56/test-api:$CAL_VER-python38
-# docker push mikeryan56/test-api:latest
+echo "Push"
+docker push mikeryan56/test-api:$CAL_VER-python38
+docker push mikeryan56/test-api:latest
 # echo 'Docker Build with PyPy'
 # docker build -t mikeryan56/test-api:$CAL_VER -f ./docker/dockerfile_pypy .
-
