ac6772fba46c406a137aeaa84f368c4e7d6375b2
devsetgo <mikeryan56@gmail.com>
2021-11-22 19:32:45 -0500

    adding errors and tests

--- src/tests/test_default.py
+++ src/tests/test_default.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # import unittest
 from unittest import TestCase
-
+import uuid
 from starlette.testclient import TestClient
 
 from src.main import app
@@ -13,6 +13,10 @@ class Test(TestCase):
     def test_index(self):
         response = client.get("/")
         assert response.status_code == 200
+
+    def test_index_not_found(self):
+        response = client.get(f"/{uuid.uuid4()}")
+        assert response.status_code == 404
 
     def test_joke(self):
         response = client.get("/joke")
@@ -26,6 +30,10 @@ class Test(TestCase):
     def test_metrics(self):
         response = client.get("/api/health/metrics")
         assert response.status_code == 200
+
+    def test_metric_not_found(self):
+        response = client.get(f"/api/health/metrics/{uuid.uuid4()}")
+        assert response.status_code == 404
 
     def test_robots(self):
         response = client.get("/robots.txt")
