1fb139b607484b0ed7cf5fa984a839e93d0f571f
devsetgo <mikeryan56@gmail.com>
2022-11-25 18:29:27 +0000

    formating and schemathesis

--- src/crud/user_crud.py
+++ src/crud/user_crud.py
@@ -10,8 +10,11 @@ from core.db_setup import users
 from crud.crud_ops import fetch_one_db, execute_one_db, fetch_all_db
 from sqlalchemy import or_, and_
 
-async def check_unique_contraints(email: str, user_name:str) -> bool:
-    query = users.select().where(or_(users.c.email == email, users.c.user_name == user_name))
+
+async def check_unique_contraints(email: str, user_name: str) -> bool:
+    query = users.select().where(
+        or_(users.c.email == email, users.c.user_name == user_name)
+    )
     result = await fetch_all_db(query=query)
     logger.error(result)
     if result is not None:
